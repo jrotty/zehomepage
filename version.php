66f63b05cb07004eb4156f88736520dc565bd744
jrotty <bssf@qq.com>
2022-10-09 12:07:54 +0800

    Update version.php

--- version.php
+++ version.php
@@ -1,10 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('Content-type:text/json'); 
-$json['ver']='0.1.1';
-$json['avatar']='https://cdn.helingqi.com/avatar/';
+$json['ver']='0.1.6';
 $json['message']='ok';
 echo json_encode($json);
-
 
 ?>
