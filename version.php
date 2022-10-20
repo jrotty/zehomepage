b59e5cd11f57e20b7c2acb4e0c300b975097e489
jrotty <bssf@qq.com>
2022-10-20 20:09:41 +0800

    修复清理缓存功能无效问题
    重新加入个内部版本号，同时在点击清除缓存时，就在版本号后追加一个递增的数字变量

--- version.php
+++ version.php
@@ -1,8 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header('Content-type:text/json'); 
-$json['ver']='0.2.0';
+header('Content-type:text/json');
+$json['ver']='20221020';
+$json['version']='0.2.1';
 $json['message']='ok';
 echo json_encode($json);
+
 
 ?>
