1ccf8c06483b867120a59854f5d413a9fbed8f68
jrotty <bssf@qq.com>
2022-10-31 16:22:11 +0800

    Update version.php

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('Content-type:text/json');
-$json['ver']='20221020';
+$json['ver']='20221026';
 $json['version']='0.2.1';
 $json['message']='ok';
 echo json_encode($json);
